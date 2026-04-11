83f8d937b280a61a8af606704de4e15d1404a8e6
Emmax08 <benitezbalta61@gmail.com>
2026-04-10 22:56:53 -0600

    Actualizar restart.js

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,12 +1,16 @@
+import fs from 'fs'
+
 export default {
   command: ['restart', 'reiniciar'],
   category: 'mod',
   isOwner: true,
   run: async (client, m) => {
-    // Mensaje de despedida antes del reinicio
     await client.reply(m.chat, `✎ Reiniciando el infierno...\n> *Prepárate para el vacío...*`, m)
+    
+    // Guardamos dónde debe avisar al despertar
+    const data = { chat: m.chat, key: m.key }
+    fs.writeFileSync('./reboot.json', JSON.stringify(data))
 
-    // Breve espera para asegurar que el mensaje se envíe antes de matar el proceso
     setTimeout(() => {
       if (process.send) {
         process.send("restart")
@@ -15,6 +19,4 @@ export default {
       }
     }, 3000)
   },
-  // Nota: El mensaje de "vuelto a encender" debe configurarse en tu archivo principal (index.js o main.js)
-  // ya que el plugin se descarga de la memoria al reiniciarse el proceso.
 };
